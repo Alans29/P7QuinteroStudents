f2436969c913131a89ad7b66b5f621360d7fedd4
aaron4pf <aaronsauceda12@gmail.com>
2023-05-18 15:24:51 -0700

    updated player controller script
    it now works sufficiently

--- P7FinalQuinteroStudents/Assets/Scripts/PlayerController.cs
+++ P7FinalQuinteroStudents/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public int speed;
-    public int brakingPower;
-    int player;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +15,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Up))
-            transform.translate(Vector3.forward * speed * Time.deltaTime);
-
-            
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
     }
 }
