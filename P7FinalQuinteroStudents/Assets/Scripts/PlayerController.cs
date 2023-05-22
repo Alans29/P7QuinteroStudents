43ebaa8dae795c141b52ebb5cd4cca132202a3e6
aaron4pf <aaronsauceda12@gmail.com>
2023-05-22 15:24:54 -0700

    changed a few box collider sizes
    ^

--- P7FinalQuinteroStudents/Assets/Scripts/PlayerController.cs
+++ P7FinalQuinteroStudents/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int speed;
+    public float verticalSpeed;
+    public float horizontalSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +17,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.UpArrow))
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            transform.Translate(Vector3.forward * verticalSpeed * Time.deltaTime);
         if (Input.GetKey(KeyCode.DownArrow))
-            transform.Translate(-Vector3.forward * speed * Time.deltaTime);
+            transform.Translate(-Vector3.forward * verticalSpeed * Time.deltaTime);
         if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            transform.Translate(Vector3.left * horizontalSpeed * Time.deltaTime);
         if (Input.GetKey(KeyCode.RightArrow))
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
     }
 }
